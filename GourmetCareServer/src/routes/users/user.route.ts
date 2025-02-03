113dc958eec88e6aa6d44d5bf8b2e513e6571b55
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2025-02-03 16:47:31 +0400

    Refactor user routes to utilize controller functions for improved user management and streamline API endpoints

--- GourmetCareServer/src/routes/users/user.route.ts
+++ GourmetCareServer/src/routes/users/user.route.ts
@@ -1,35 +1,13 @@
+import {createUser, getUserById, getUserByEmail, updateUser, deleteUser } from '../../controllers/users/user.controller'; 
+
 import { Router } from 'express';
-import { Request, Response } from 'express';
-import UserController from '../../controllers/users/user.controller';
 
-const router: Router = Router();
+const router = Router();
 
-// Register new user
-router.post('/register', async (req: Request, res: Response) => {
-  await UserController.register(req, res);
-});
-
-// Login user  
-router.post('/login', async (req: Request, res: Response) => {
-  await UserController.login(req, res);
-});
-
-// Logout user
-router.post('/logout', async (req: Request, res: Response) => {
-  await UserController.logout(req, res);
-});
-
-// Update user details
-router.put('/:id', async (req: Request, res: Response) => {
-  await UserController.updateUser(req, res);
-});
-
-router.get('/all', async (req: Request, res: Response) => {
-    try {
-        res.status(200).json({ users: "All users" });
-    } catch (error) {
-        res.status(500).json({ message: 'Error getting all users', error });
-    }
-});
+router.post('/user', createUser);
+router.get('/user', getUserByEmail);
+router.get('/user/:id', getUserById);
+router.put('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
 
 export default router;
